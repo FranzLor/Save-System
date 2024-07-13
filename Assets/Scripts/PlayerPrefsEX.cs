bc792909da43b5c3405d27d2455290f37ffbd898
Franz <FranzLor64@yahoo.com>
2024-07-12 23:03:22 -0600

    Player Prefs: Testing
    testing player prefs int, float, and string info using "A" and "D"

--- Assets/Scripts/PlayerPrefsEX.cs
+++ Assets/Scripts/PlayerPrefsEX.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerPrefsEX : MonoBehaviour
 {
+    public int aInt;
+    public float aFloat;
+    public string aString;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,24 @@ public class PlayerPrefsEX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //save
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Save");
+
+            PlayerPrefs.SetInt("testInt", aInt);
+            PlayerPrefs.SetFloat("testFloat", aFloat);
+            PlayerPrefs.SetString("testString", aString);
+        }
+
+        //load
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Load");
+
+            aInt = PlayerPrefs.GetInt("testInt");
+            aFloat = PlayerPrefs.GetFloat("testFloat");
+            aString = PlayerPrefs.GetString("testString");
+        }
     }
 }
